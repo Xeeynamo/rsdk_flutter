1ca8010d800ff1bcea19ccdfd16753b6b9be5888
Luciano Ciccariello <xeeynamo@hotmail.com>
2021-10-14 19:27:53 +0100

    Add camera scrolling

--- lib/levelview.dart
+++ lib/levelview.dart
@@ -1,29 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:rsdk_flutter/painters/stagepainter.dart';
 import 'models/stages/stage.dart';
 
-class LevelView extends StatelessWidget {
+class LevelView extends StatefulWidget {
   final Stage stage;
 
-  const LevelView({Key? key, required this.stage}) : super(key: key);
+  LevelView({Key? key, required this.stage}) : super(key: key);
 
-  int get stageWidth => stage.act.width;
-  int get stageHeight => stage.act.height;
+  @override
+  State<StatefulWidget> createState() => _LevelViewState(stage);
+}
+
+class _LevelViewState extends State<LevelView> {
+  final Stage stage;
+  Offset _offset = Offset.zero;
+
+  _LevelViewState(this.stage);
+
+  Offset get offset => _offset;
+  set offset(Offset value) => setState(() {
+        _offset = Offset(_bound(value.dx, stage.act.width),
+            _bound(value.dy, stage.act.height));
+      });
+
+  @override
+  initState() {
+    _offset = Offset.zero;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: SizedBox(
-          width: stageWidth * Stage.chunkSize / 1,
-          height: stageHeight * Stage.chunkSize / 1,
-          child: _paintStageForeground(context),
-        ),
-        scrollDirection: Axis.horizontal);
+    return Stack(fit: StackFit.expand, children: <Widget>[
+      Draggable(
+        child: _paintStageForeground(context),
+        feedback: const SizedBox.shrink(),
+        onDragUpdate: (details) => offset -= details.delta,
+      )
+    ]);
   }
 
-  Widget _paintStageForeground(BuildContext context) {
-    return CustomPaint(
-      painter: StagePainter(stage),
-    );
-  }
+  Widget _paintStageForeground(BuildContext context) =>
+      CustomPaint(painter: StagePainter(stage, _offset));
+
+  double _bound(double value, int chunkBoundary) =>
+      min(max(value, 0), chunkBoundary * 128);
 }
--- lib/painters/stagepainter.dart
+++ lib/painters/stagepainter.dart
@@ -5,24 +5,30 @@ import 'package:rsdk_flutter/models/stages/stage.dart';
 class StagePainter extends widgets.CustomPainter {
   static const chunkSize = 128.0;
   final Stage _stage;
+  final Offset offset;
 
-  StagePainter(this._stage);
+  StagePainter(this._stage, this.offset);
 
   @override
   void paint(Canvas canvas, Size size) {
     final imagePaint = Paint();
-    for (var y = 0; y < size.height && y < _stage.act.height; y++) {
-      for (var x = 0; x < size.width && x < _stage.act.width; x++) {
-        final chunkId = _stage.act.getChunkId(x, y);
-        canvas.drawImage(_stage.chunksImage[chunkId], Offset(x * chunkSize, y * chunkSize), imagePaint);
+    final sizeHeight = (size.height + chunkSize * 2 - 1) ~/ chunkSize;
+    final sizeWidth = (size.width + chunkSize * 2 - 1) ~/ chunkSize;
+    final yMod = offset.dy ~/ chunkSize;
+    final xMod = offset.dx ~/ chunkSize;
+    final yOff = offset.dy % chunkSize;
+    final xOff = offset.dx % chunkSize;
+    for (var y = 0; y < sizeHeight && (yMod + y) < _stage.act.height; y++) {
+      if (yMod + y < 0) continue;
+      for (var x = 0; x < sizeWidth && (xMod + x) < _stage.act.width; x++) {
+        if (xMod + x < 0) continue;
+        final chunkId = _stage.act.getChunkId(xMod + x, yMod + y);
+        canvas.drawImage(_stage.chunksImage[chunkId],
+            Offset(x * chunkSize - xOff, y * chunkSize - yOff), imagePaint);
       }
     }
   }
 
   @override
-  bool shouldRepaint(covariant widgets.CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    return false;
-  }
-
-} 
+  bool shouldRepaint(covariant widgets.CustomPainter oldDelegate) => true;
+}
